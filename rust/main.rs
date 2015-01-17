98b5067e65f1f347f6782119afb5b1042ce724f9
Ben Foppa <benjamin.foppa@gmail.com>
2015-01-17 15:24:03 -0500

    rustup

--- rust/main.rs
+++ rust/main.rs
@@ -3,31 +3,32 @@ use opencl::mem::CLBuffer;
 use std::io::File;
 use std::str;
 
-static WINDOW_WIDTH: uint = 800;
-static WINDOW_HEIGHT: uint = 600;
+static WINDOW_WIDTH: usize = 800;
+static WINDOW_HEIGHT: usize = 600;
 
 pub fn main() {
-  let path = Path::new("opencl/add.ocl");
-  let ker = File::open(&path).read_to_end().unwrap();
-  let ker = str::from_utf8(ker.as_slice()).unwrap();
-
   let (device, ctx, queue) = opencl::util::create_compute_context().unwrap();
 
   let len = WINDOW_WIDTH * WINDOW_HEIGHT;
 
-  let x_values: Vec<uint> =
+  let x_values: Vec<usize> =
     range(0, len).map(|i| i % WINDOW_WIDTH).collect();
-  let y_values: Vec<uint> =
+  let y_values: Vec<usize> =
     range(0, len).map(|i| i / WINDOW_WIDTH).collect();
 
-  let x_value_buffer: CLBuffer<uint> = ctx.create_buffer(len, opencl::CL::CL_MEM_READ_ONLY);
-  let y_value_buffer: CLBuffer<uint> = ctx.create_buffer(len, opencl::CL::CL_MEM_READ_ONLY);
-  let output_buffer: CLBuffer<uint> = ctx.create_buffer(len, opencl::CL::CL_MEM_WRITE_ONLY);
+  let x_value_buffer: CLBuffer<usize> = ctx.create_buffer(len, opencl::cl::CL_MEM_READ_ONLY);
+  let y_value_buffer: CLBuffer<usize> = ctx.create_buffer(len, opencl::cl::CL_MEM_READ_ONLY);
+  let output_buffer: CLBuffer<usize> = ctx.create_buffer(len, opencl::cl::CL_MEM_WRITE_ONLY);
 
   queue.write(&x_value_buffer, &x_values.as_slice(), ());
   queue.write(&y_value_buffer, &y_values.as_slice(), ());
 
-  let program = ctx.create_program_from_source(ker);
+  let path = Path::new("opencl/add.ocl");
+  let ker = File::open(&path).read_to_end().unwrap();
+  let program = {
+    let ker = str::from_utf8(ker.as_slice()).unwrap();
+    ctx.create_program_from_source(ker)
+  };
   program.build(&device).ok().expect("Couldn't build program.");
 
   let kernel = program.create_kernel("vector_add");
@@ -37,6 +38,6 @@ pub fn main() {
 
   let event = queue.enqueue_async_kernel(&kernel, len, None, ());
 
-  let vec_c: Vec<uint> = queue.get(&output_buffer, &event);
-  println!("results: {}", vec_c);
+  let vec_c: Vec<usize> = queue.get(&output_buffer, &event);
+  println!("results: {:?}", vec_c);
 }
