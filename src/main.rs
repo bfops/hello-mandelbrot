83b4acbe420d7e8690b72bda5f7663c26b5fb94c
Zeke Foppa <benjamin.foppa@gmail.com>
2015-08-02 12:05:09 -0400

    Fix shader? Might be broken on lower OpenGL versions..

--- src/main.rs
+++ src/main.rs
@@ -114,9 +114,10 @@ fn make_shader<'a, 'b:'a>(
     #version 330 core
 
     in vec4 v_color;
+    layout(location=0) out vec4 frag_color;
 
     void main() {
-      gl_FragColor = v_color;
+      frag_color = v_color;
     }
   ".to_string();
 
